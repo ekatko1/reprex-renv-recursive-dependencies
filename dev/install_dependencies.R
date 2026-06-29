2048e8413a8f9772e1d4d9992e1d7860cac04bf6
ekatko1 <26992969+ekatko1@users.noreply.github.com>
2026-06-29 15:08:24 -0400

    fix pkg install script

--- dev/install_dependencies.R
+++ dev/install_dependencies.R
@@ -1,4 +1,5 @@
 ## Run in GitHub Actions
 install.packages("pak")
-pak::pkg_install(renv, box)
-renv::dependencies("Report.qmd")
+pak::pkg_install(c("renv", "box", "rmarkdown"))
+renv::dependencies("Report.qmd")$Package |> 
+  pak::pkg_install()
